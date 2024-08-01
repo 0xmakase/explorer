fdb30d230e98207d1220b9813978cc7d40dda9af
Kenjii <kenjii@0xmakase.co.jp>
2024-08-01 15:12:49 +0900

    add lava mainnet

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,9 @@
 const dymensionFunctions = require("./mainnet/dymension/index");
 const seiFunctions = require("./mainnet/sei/index");
 const lavaFunctions = require("./mainnet/lava/index");
-const lavaTestFunctions = require("./testnet/lava/index");
 
 module.exports = {
   ...dymensionFunctions,
   ...seiFunctions,
   ...lavaFunctions,
-  ...lavaTestFunctions,
 };
