bf8176adfdbcbce618e75b2c35575a67824fba86
Kenjii <kenjii@0xmakase.co.jp>
2024-10-01 15:38:27 +0900

    initiaのURL追加

--- functions/testnet/initia/index.js
+++ functions/testnet/initia/index.js
@@ -1,8 +1,8 @@
 const functions = require("firebase-functions");
 const createApp = require("../../common/createApp");
 
-const apiApp = createApp("");
-const rpcApp = createApp("");
+const apiApp = createApp("http://initia-testnet-initiation-2-nlb-7285263470cec22b.elb.eu-central-1.amazonaws.com:14617");
+const rpcApp = createApp("http://initia-testnet-initiation-2-nlb-7285263470cec22b.elb.eu-central-1.amazonaws.com:26657");
 
 exports.proxyToSentryinitiaTestApi = functions.https.onRequest(apiApp);
 exports.proxyToSentryinitiaTestRpc = functions.https.onRequest(rpcApp);
