2657819b6364357d6658226f6bf67b76ade9928d
Kenjii <kenjii@0xmakase.co.jp>
2024-10-17 14:40:09 +0900

    lava削除

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,11 @@
 const dymensionFunctions = require("./mainnet/dymension/index");
 const seiFunctions = require("./mainnet/sei/index");
-const lavaFunctions = require("./mainnet/lava/index");
 const initiaFunctions = require("./testnet/initia/index");
 const bouncebitFunctions = require("./mainnet/bouncebit/index");
 
 module.exports = {
   ...dymensionFunctions,
   ...seiFunctions,
-  ...lavaFunctions,
   ...initiaFunctions,
   ...bouncebitFunctions,
 };
