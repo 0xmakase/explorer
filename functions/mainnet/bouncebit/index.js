0f84a7efc5271d0386f194192fc18917180db507
Kenjii <kenjii@0xmakase.co.jp>
2024-10-31 09:20:52 +0900

    Remove unused mainnet chains and related functions

--- functions/mainnet/bouncebit/index.js
+++
@@ -1,8 +0,0 @@
-const functions = require("firebase-functions");
-const createApp = require("../../common/createApp");
-
-const apiApp = createApp("http://bouncebit-mainnet-rpc-nlb-2664d095af56ec6a.elb.ap-northeast-1.amazonaws.com:14617");
-const rpcApp = createApp("http://bouncebit-mainnet-rpc-nlb-2664d095af56ec6a.elb.ap-northeast-1.amazonaws.com:26657");
-
-exports.proxyToSentryBBApi = functions.https.onRequest(apiApp);
-exports.proxyToSentryBBRpc = functions.https.onRequest(rpcApp);
